85bde808e5cdc075adc9aa5790fde62afd6ec3b2
olympia <owojcik7@gmail.com>
2020-02-06 15:25:10 -0500

    Create Routes & Responses in Express

--- devcamper-api/server.js
+++ devcamper-api/server.js
@@ -7,6 +7,28 @@ dotenv.config({ path: './config/config.env' })
 
 const app = express()
 
+app.get('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json({ success: true, msg: "Show all bootcamps" })
+})
+
+//Get single bootcamp
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({ success: true, msg: `Show a single bootcamp ${req.params.id}` })
+})
+
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({ success: true, msg: `Update bootcamp ${req.params.id}` })
+})
+
+app.post('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json({ success: true, msg: "Create new bootcamp" })
+})
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({ success: true, msg: `Delete bootcamp ${req.params.id}` })
+})
+
 
 const PORT = process.env.PORT || 5000
 
