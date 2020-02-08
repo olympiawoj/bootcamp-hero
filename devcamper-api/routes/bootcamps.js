e13a7c9a3f59fc1b95418ee8be92d21597325e47
olympia <owojcik7@gmail.com>
2020-02-07 21:39:20 -0500

    Create controller methods

--- devcamper-api/routes/bootcamps.js
+++ devcamper-api/routes/bootcamps.js
@@ -1,27 +1,22 @@
 const express = require('express')
+const {
+    getBootcamps,
+    getBootcamp,
+    createBootcamp,
+    updateBootcamp,
+    deleteBootcamp
+} = require('../controllers/bootcamps')
 const router = express.Router()
 
+router
+    .route('/')
+    .get(getBootcamps)
+    .post(createBootcamp)
 
-router.get('/', (req, res) => {
-    res.status(200).json({ success: true, msg: "Show all bootcamps" })
-})
-
-//Get single bootcamp
-router.get('/:id', (req, res) => {
-    res.status(200).json({ success: true, msg: `Show a single bootcamp ${req.params.id}` })
-})
-
-
-router.put('/:id', (req, res) => {
-    res.status(200).json({ success: true, msg: `Update bootcamp ${req.params.id}` })
-})
-
-router.post('/', (req, res) => {
-    res.status(200).json({ success: true, msg: "Create new bootcamp" })
-})
-
-router.delete('/:id', (req, res) => {
-    res.status(200).json({ success: true, msg: `Delete bootcamp ${req.params.id}` })
-})
+router
+    .route('/:id')
+    .get(getBootcamp)
+    .put(updateBootcamp)
+    .delete(deleteBootcamp)
 
 module.exports = router
