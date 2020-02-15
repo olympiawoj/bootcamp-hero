5f4670d9b5f82e73d9d4f1a7bea95fbac7efc2c6
olympia <owojcik7@gmail.com>
2020-02-15 17:48:54 -0500

    Connect to the Database with Mongoose

--- devcamper-api/server.js
+++ devcamper-api/server.js
@@ -6,8 +6,12 @@ const morgan = require('morgan')
 const bootcamps = require('./routes/bootcamps')
 const dotenv = require('dotenv')
 
+const connectDB = require('./config/db')
+
 //Load env vars
 dotenv.config({ path: './config/config.env' })
+//Connect to database
+connectDB()
 
 const app = express()
 
@@ -22,4 +26,11 @@ app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in, ${process.env.NODE_ENV} mode on port ${PORT}`))
+const server = app.listen(PORT, console.log(`Server running in, ${process.env.NODE_ENV} mode on port ${PORT}`))
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`)
+    //Close server and exit process
+    server.close(() => process.exit(1))
+})
