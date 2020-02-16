51bf22d1077fa34e29c5c2ddc1146ced97fe8ffe
olympia <owojcik7@gmail.com>
2020-02-16 13:19:19 -0500

    Install Colors and Add Colors in the Console

--- devcamper-api/server.js
+++ devcamper-api/server.js
@@ -1,6 +1,7 @@
 
 const express = require('express')
 const morgan = require('morgan')
+const colors = require('colors')
 
 //Route files
 const bootcamps = require('./routes/bootcamps')
@@ -26,11 +27,11 @@ app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000
 
-const server = app.listen(PORT, console.log(`Server running in, ${process.env.NODE_ENV} mode on port ${PORT}`))
+const server = app.listen(PORT, console.log(`Server running in, ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
 
 //Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-    console.log(`Error: ${err.message}`)
+    console.log(`Error: ${err.message}`.red)
     //Close server and exit process
     server.close(() => process.exit(1))
 })
