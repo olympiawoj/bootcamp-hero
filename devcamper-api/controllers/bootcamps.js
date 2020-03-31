72d0ce95d3c270a50b21fe0b8da99a2f80d7b443
olympia <owojcik7@gmail.com>
2020-03-31 09:37:58 -0400

    Add ability to Create Bootcamp via POST request

--- devcamper-api/controllers/bootcamps.js
+++ devcamper-api/controllers/bootcamps.js
@@ -1,3 +1,4 @@
+const Bootcamp = require('../models/Bootcamp')
 
 //@desc Get all bootcamps
 //@route GET /api/v1/bootcamps
@@ -18,8 +19,18 @@ exports.getBootcamp = (req, res, next) => {
 //@desc Create new bootcamp
 //@route POST /api/v1/bootcamps
 //@access Private aka must be logged in or send token
-exports.createBootcamp = (req, res, next) => {
-    res.status(200).json({ success: true, msg: "Create new bootcamp" })
+exports.createBootcamp = async (req, res, next) => {
+    // console.log(req.body)
+    // res.status(200).json({ success: true, msg: "Create new bootcamp" })
+    try {
+        const bootcamp = await Bootcamp.create(req.body)
+        res.status(201).json({
+            success: true,
+            data: bootcamp
+        })
+    } catch (err) {
+        res.status(400).json({ success: false })
+    }
 }
 
 
--- devcamper-api/server.js
+++ devcamper-api/server.js
@@ -16,6 +16,9 @@ connectDB()
 
 const app = express()
 
+// Body parser
+app.use(express.json())
+
 //Dev logging middleware
 if (process.env.NODE_ENV === "development") {
     app.use(morgan('dev'))
