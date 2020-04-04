e51aec04c6cce4cd965178d65827ea5764c12f2d
olympia <owojcik7@gmail.com>
2020-04-04 19:56:38 -0400

    Add Updating & Deleting Bootcamp Methods - PUT & DELETE

--- devcamper-api/controllers/bootcamps.js
+++ devcamper-api/controllers/bootcamps.js
@@ -6,7 +6,7 @@ const Bootcamp = require('../models/Bootcamp')
 exports.getBootcamps = async (req, res, next) => {
     try {
         const bootcamps = await Bootcamp.find()
-        res.status(200).json({ success: true, data: bootcamps })
+        res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps })
     }
     catch (err) {
         res.status(400).json({ success: false })
@@ -53,8 +53,21 @@ exports.createBootcamp = async (req, res, next) => {
 //@desc Update bootcamp
 //@route PUT /api/v1/bootcamps/:id
 //@access Private
-exports.updateBootcamp = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Update bootcamp ${req.params.id}` })
+exports.updateBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        })
+
+        if (!bootcamp) {
+            return res.status(400).json({ success: false })
+        }
+        res.status(200).json({ success: true, data: bootcamp })
+    }
+    catch (err) {
+        res.status(400).json({ success: false })
+    }
 }
 
 
@@ -62,6 +75,16 @@ exports.updateBootcamp = (req, res, next) => {
 //@desc Delete single bootcamps
 //@route DELETE /api/v1/bootcamps/:id
 //@access Private
-exports.deleteBootcamp = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Delete bootcamp ${req.params.id}` })
+exports.deleteBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id)
+
+        if (!bootcamp) {
+            return res.status(400).json({ success: false })
+        }
+        res.status(200).json({ success: true, data: {} })
+    }
+    catch (err) {
+        res.status(400).json({ success: false })
+    }
 }
