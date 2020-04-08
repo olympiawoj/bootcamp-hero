9977c4bd49d43715e08fab554cff48bdfca07cb9
olympia <owojcik7@gmail.com>
2020-04-08 13:42:26 -0400

    Add Mongoose Middleware & Slugify

--- devcamper-api/models/Bootcamp.js
+++ devcamper-api/models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const slugify = require('slugify')
 
 const BootcampSchema = new mongoose.Schema({
     //fields 
@@ -110,5 +111,12 @@ const BootcampSchema = new mongoose.Schema({
 
 },
 )
+
+// Create bootcamp slug from the name 
+BootcampSchema.pre('save', function (next) {
+    this.slug = slugify(this.name, { lower: true })
+    next()
+})
+
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema)
