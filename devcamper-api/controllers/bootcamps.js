ef5751cc23a02da81b30c7dc37c158267cb913a0
olympia <owojcik7@gmail.com>
2020-04-13 18:00:59 -0400

    Add ability to Select & Sort our getBootcamps request in controllers/bootcamps.js

--- devcamper-api/controllers/bootcamps.js
+++ devcamper-api/controllers/bootcamps.js
@@ -9,12 +9,41 @@ const Bootcamp = require('../models/Bootcamp')
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
     let query;
 
-    let queryStr = JSON.stringify(req.query) //query param as json string
+    // Copy req.query
+    const reqQuery = { ...req.query }
 
+    // Fields to exclude
+    const removeFields = ['select', 'sort']
+    //Loop over removeFields and delete them from reqQuery 
+    removeFields.forEach(param => delete reqQuery[param])
+
+    // Create query string
+    let queryStr = JSON.stringify(reqQuery) //query param as json string
+
+    // Create operators ($gt, $gte, etc)
     queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`)//1st param-replace takes in regex, g=global means doesnt stop at the first it finds, 2nd param- takes in a function, match => what to return?
 
+    // Finding resource 
     query = Bootcamp.find(JSON.parse(queryStr))
 
+    // Build & Sort adds onto our query
+    // Select Fields- do this if select is included
+    if (req.query.select) {
+        const fields = req.query.select.split(',').join(' ')
+        query = query.select(fields)
+    }
+    // Sort
+    if (req.query.sort) {
+        //sort by multiple comma separated field values then turned into spaced strings
+        const sortBy = req.query.sort.split(',').join(' ')
+        query = query.sort(sortBy)
+    } else {
+        //default sort, descending created at
+        query = query.sort('-createdAt')
+
+    }
+
+    // Executing our query
     const bootcamps = await query
     res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps })
 
@@ -33,6 +62,7 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
         //is a formatted object id BUT it's not in the database
         return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404))
     }
+
     res.status(200).json({ success: true, data: bootcamp })
 
 })
