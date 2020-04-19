3e44e97c07fd2f093194b091ca5cc7e4ec56f879
olympia <owojcik7@gmail.com>
2020-04-19 15:29:43 -0400

    Add ability to Get Single Course & Add Course

--- devcamper-api/controllers/courses.js
+++ devcamper-api/controllers/courses.js
@@ -1,6 +1,7 @@
 const ErrorResponse = require('../utils/errorResponse')
 const asyncHandler = require('../middleware/async')
 const Course = require('../models/Course')
+const Bootcamp = require('../models/Bootcamp')
 
 //@desc Get courses
 //@route GET /api/v1/courses
@@ -28,5 +29,53 @@ exports.getCourses = asyncHandler(async (req, res) => {
         success: true,
         count: courses.length,
         data: courses
+    })
+})
+
+//@desc Get a single course
+//@route GET /api/v1/courses/:id
+//@access Public
+
+
+exports.getCourse = asyncHandler(async (req, res) => {
+    console.log("res", res)
+    //populate to show bootcamp name and description 
+    const course = await Course.findById(req.params.id).populate({
+        path: 'bootcamp',
+        select: 'name description'
+    })
+
+    if (!course) {
+        return next(new ErrorResponse(`No course with the id of ${req.params.id}`), 404)
+    }
+
+    res.status(200).json({
+        success: true,
+        data: course
+    })
+})
+
+
+//@desc Add course
+//@route POST /api/v1/bootcamps/:bootcampId/courses 
+//@access Private - to add a new course, only a logged in user should be able to do this
+
+exports.addCourse = asyncHandler(async (req, res) => {
+
+    req.body.bootcamp = req.params.bootcampId
+
+    const bootcamp = await Bootcamp.findById(req.params.bootcampId)
+
+    //check to see if the bootcamp exists
+    if (!bootcamp) {
+        return next(new ErrorResponse(`No bootamp with the id of ${req.params.bootcampId}`), 404)
+    }
+
+    //create new course- req.body includes anything we include in the body including or bootcampId which we pulled out of the URL 
+    const course = await Course.create(req.body)
+
+    res.status(200).json({
+        success: true,
+        data: course
     })
 })
--- devcamper-api/routes/courses.js
+++ devcamper-api/routes/courses.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const {
-    getCourses
+    getCourses,
+    getCourse,
+    addCourse
 } = require('../controllers/courses')
 
 const router = express.Router({ mergeParams: true })
 
-router.route('/').get(getCourses);
+router.route('/').get(getCourses).post(addCourse)
+router.route('/:id').get(getCourse)
+
+
 
 
 module.exports = router 
