cfb73f996e3aa18d5c8d14a934ed51f26b3edcdd
olympia <owojcik7@gmail.com>
2020-05-06 12:01:16 -0400

    Implement Forgot Password - Send Email functionality

--- devcamper-api/controllers/auth.js
+++ devcamper-api/controllers/auth.js
@@ -1,4 +1,5 @@
 const ErrorResponse = require('../utils/errorResponse')
+const sendEmail = require('../utils/sendEmail')
 const asyncHandler = require('../middleware/async')
 const User = require('../models/User')
 const getSignedJwtToken = require('../')
@@ -100,10 +101,36 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
     // Save user
     await user.save({ validateBeforeSave: false })
 
-    res.status(200).json({
-        success: true,
-        data: user
-    });
+    // Create reset url
+    const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/resetpassword/${resetToken}`
+
+    // Create message to pass in
+    const message = `You are recieving this email because you (or someone else) has requested the reset of a password. Please make a PUT request to: \n\n ${resetUrl}`;
+
+    // Call send email function from utility
+    try {
+        //sendEmail takes in an object of options
+        await sendEmail({
+            email: user.email,
+            subject: "Password reset token",
+            message
+        })
+
+        return res.status(200).json({ success: true, data: 'Email sent!' })
+
+    } catch (err) {
+        //If something goes wrong, get rid of tokens in db
+        console.log(err)
+        user.resetPasswordToken = undefined
+        user.resetPasswordExpire = undefined
+        await user.save({ validateBeforeSave: false })
+        return next(new ErrorResponse('Email could not be sent', 500))
+    }
+
+    // res.status(200).json({
+    //     success: true,
+    //     data: user
+    // });
 })
 
 
