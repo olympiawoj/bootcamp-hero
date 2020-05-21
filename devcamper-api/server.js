e6367a688b28921f26f12ff868436a212a1e57f0
olympia <owojcik7@gmail.com>
2020-05-21 19:34:51 -0400

    Implement XSS Protection & Security Headers with middleware in server.js

--- devcamper-api/server.js
+++ devcamper-api/server.js
@@ -8,6 +8,8 @@ const colors = require('colors')
 const fileupload = require('express-fileupload')
 const cookieParser = require('cookie-parser')
 const mongoSanitize = require('express-mongo-sanitize')
+const helmet = require('helmet')
+const xss = require('xss-clean')
 
 const errorHandler = require('./middleware/error')
 
@@ -41,6 +43,12 @@ app.use(fileupload())
 
 // Sanitize data
 app.use(mongoSanitize())
+
+// Prevent XSS attacks
+app.use(xss())
+
+// Set security headers
+app.use(helmet())
 
 // Set public as our static folder 
 app.use(express.static(path.join(__dirname, 'public')))
